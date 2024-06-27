15e81e5d2acb57723b041dc71d7a50067c155058
rodolfo <cantillanarodolf@gmail.com>
2024-06-26 22:05:27 -0400

    nice

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: '*', 
+  origin: ['*','https://backend-quejas-production.up.railway.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Credentials', 'source']
