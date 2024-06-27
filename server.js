3c4ff6555d8d092b6503faaab7acf70435856628
rodolfo <cantillanarodolf@gmail.com>
2024-06-26 22:41:09 -0400

    now

--- server.js
+++ server.js
@@ -35,7 +35,15 @@ const corsOptions = {
 
 app.options('*', cors(corsOptions)); // Enable preflight response for all routes
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, source");
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/support', supportRouter);
